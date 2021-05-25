83565c7ac799916324e0e04e1eccbdf6817f4b79
Max Silchenko <maksim379@yandex.ru>
2021-05-25 23:37:29 +0300

    Выполненное задание

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -12,9 +12,10 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
-import logging
+import logging, ephem, datetime
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import date
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -36,24 +37,57 @@ def greet_user(update, context):
     update.message.reply_text(text)
 
 
+def planet(update, context):
+    planet = update.message.text.split()
+    if "mercury" in planet:
+      mercury = ephem.Mercury(datetime.date.today())
+      planet = ephem.constellation(mercury)
+    else:
+      if "venus"in planet:
+        venus = ephem.Venus(datetime.date.today())
+        planet = ephem.constellation(venus)
+      else:
+          if 'earth' in planet:
+            earth = ephem.Earth(datetime.date.today())
+            planet = ephem.constellation(earth)
+          else:
+              if 'mars'in planet:
+                mars = ephem.Mars(datetime.date.today())
+                planet = ephem.constellation(mars)
+              else:
+                  if 'jupiter' in planet:
+                    jupiter = ephem.Jupiter(datetime.date.today())
+                    planet = ephem.constellation(jupiter)
+                  else:
+                      if 'saturn' in planet:
+                        saturn = ephem.Saturn(datetime.date.today())
+                        planet = ephem.constellation(saturn)
+                      else:
+                          if 'uranus' in planet:
+                            uranus = ephem.Uranus(datetime.date.today())
+                            planet = ephem.constellation(uranus)
+                          else:
+                              if 'neptune'in planet:
+                                neptune = ephem.Neptune(datetime.date.today())
+                                planet = ephem.constellation(neptune)
+                              else:
+                                  planet = 'unknown planet'
+     
+    update.message.reply_text(planet)
+    
+    
 def talk_to_me(update, context):
     text = update.message.text
     print(text)
     update.message.reply_text(text)
 
 
-def planet(update, context):
-    text = 'начали!'
-    print(text)
-
-
 def main():
     mybot = Updater("1887880943:AAHVX6-ECQ5RpoWBuMsN9Y5PhGh0rwuHwow", use_context=True)
-
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     dp.add_handler(CommandHandler("planet", planet))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
     mybot.idle()
